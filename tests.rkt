de6b19a018e235a8ada4147dc179131e47a73847
Srihari Ramanathan <srihari.ramanathan@gmail.com>
2010-10-08 20:41:40 +0530

    added the Deutsch-Jozsa algorithm

--- tests.rkt
+++ tests.rkt
@@ -9,3 +9,13 @@
     (q:hadamard 0 reg)
     (q:bmeasure 0 reg)))
 
+; the Deutsch-Jozsa algorithm, from
+; http://www.linux-magazin.de/layout/set/print/content/view/full/21495
+(define (deutsch-jozsa fun)
+  (let ([reg (q:new-qureg 5 3)])
+    (for ([i (in-range 3)])
+      (q:hadamard i reg))
+    (fun reg)
+    (for ([i (in-range 2)])
+      (q:hadamard i reg))
+    (q:bmeasure 0 reg)))
